979425b353d1aa3cc8f899161ce7c0f846949d87
Mohana-Siva <mohanas.23csd@kongu.edu>
2026-07-21 16:09:41 +0530

    week-5&6

--- week5/blogapp/src/UrlTest.js
+++
@@ -1,6 +0,0 @@
-const UrlTest = async () =>{
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const result = await res.json();
-    console.log(result);
-}
-UrlTest();
